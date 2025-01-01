e00f35f333156ea56b5306780a40e26c325a00cf
asiaeiwa <82070692+asiaeiwa@users.noreply.github.com>
2025-01-01 12:53:01 +0700

    feat: add namecard folder (#10)
    Co-authored-by: Uy Pham <uyphamvuong@gmail.com>

---
+++ src/pages/namecard/nga/index.tsx
@@ -0,0 +1,23 @@
+import { Page } from 'components/page';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { FC } from 'react';
+// import { useTranslation } from 'react-i18next';
+import styles from '../detail.module.scss';
+
+const NameCardDetail: FC = () => {
+  // const { t } = useTranslation('namecard');
+
+  return (
+    <Page id="namecared-detail" className={styles['namecard-detail']} title="Namecard: Ms.Nga">
+      Namecard: Ms.Nga
+    </Page>
+  );
+};
+
+export const getStaticProps = async ({ locale }: { locale: string }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['common', 'namecard'])),
+  },
+});
+
+export default NameCardDetail;
