86cefc821aba4902c1a860c590703382cd56c656
Uy Pham <uyphamvuong@gmail.com>
2025-01-01 12:50:32 +0700

    feat: add namecard folder

---
+++ src/pages/namecard/anh/index.tsx
@@ -0,0 +1,23 @@
+import { Page } from 'components/page';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { FC } from 'react';
+// import { useTranslation } from 'react-i18next';
+import styles from '../detail.module.scss';
+
+const NameCardDetail: FC = () => {
+  // const { t } = useTranslation('namecard');
+
+  return (
+    <Page id="namecared-detail" className={styles['namecard-detail']} title="Namecard: Ms.Anh">
+      Namecard: Ms.Anh
+    </Page>
+  );
+};
+
+export const getStaticProps = async ({ locale }: { locale: string }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['common', 'namecard'])),
+  },
+});
+
+export default NameCardDetail;
