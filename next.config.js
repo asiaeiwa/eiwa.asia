528cd063112f5a070ab861888394f53ff8f08e94
Uy Pham <uyphamvuong@gmail.com>
2025-03-17 12:35:16 +0700

    release: merge latest version

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const isFullClass = (!isProduction && process.env.ASBUILD !== 'true') || process.env.FULLCLASS === 'true';
 const isAnalyze = process.env.ANALYZE === 'true';
 const isTSCheck = process.env.TSCHECK || process.env.TSCHECK === 'true';
-const hashLength = process.env.HASHLENGTH || 6;
+const hashLength = process.env.HASHLENGTH || 8;
 
 const mapHash = { fisrt: '' };
 
